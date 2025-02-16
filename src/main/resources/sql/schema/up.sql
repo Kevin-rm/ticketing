d0b8017cb5792e4caf988d187ac9ea6fcd21b2af
Kevin-rm <kevinramarozatovo@gmail.com>
2025-02-17 00:10:03 +0300

    Update up.sql

---
+++ src/main/resources/sql/schema/up.sql
@@ -0,0 +1,114 @@
+CREATE TABLE plane
+(
+    id               SERIAL PRIMARY KEY,
+    model            VARCHAR(75) NOT NULL,
+    manufacture_date DATE        NOT NULL
+);
+
+CREATE TABLE city
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(75) NOT NULL
+);
+
+CREATE TABLE flight
+(
+    id                  SERIAL PRIMARY KEY,
+    departure_timestamp TIMESTAMP NOT NULL,
+    arrival_timestamp   TIMESTAMP NOT NULL,
+    departure_city_id   INTEGER   NOT NULL,
+    arrival_city_id     INTEGER   NOT NULL,
+    plane_id            INTEGER   NOT NULL,
+    FOREIGN KEY (departure_city_id) REFERENCES city (id),
+    FOREIGN KEY (arrival_city_id) REFERENCES city (id),
+    FOREIGN KEY (plane_id) REFERENCES plane (id)
+);
+
+CREATE TABLE seat_type
+(
+    id          SERIAL PRIMARY KEY,
+    designation VARCHAR(75) NOT NULL
+);
+
+CREATE TABLE _user
+(
+    id        SERIAL PRIMARY KEY,
+    firstname VARCHAR(75)  NOT NULL,
+    lastname  VARCHAR(75)  NOT NULL,
+    email     VARCHAR(255) NOT NULL UNIQUE,
+    password  VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE _role
+(
+    id    SERIAL PRIMARY KEY,
+    value VARCHAR(75) NOT NULL
+);
+
+CREATE TABLE user_role
+(
+    id      SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    role_id INTEGER NOT NULL,
+    UNIQUE (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES _user (id),
+    FOREIGN KEY (role_id) REFERENCES _role (id)
+);
+
+CREATE TABLE plane_seat
+(
+    id           SERIAL PRIMARY KEY,
+    seats_count  INTEGER NOT NULL DEFAULT 0,
+    plane_id     INTEGER NOT NULL,
+    seat_type_id INTEGER NOT NULL,
+    FOREIGN KEY (plane_id) REFERENCES plane (id),
+    FOREIGN KEY (seat_type_id) REFERENCES seat_type (id)
+);
+
+CREATE TABLE seat_pricing
+(
+    id            SERIAL PRIMARY KEY,
+    unit_price    NUMERIC(10, 2) NOT NULL,
+    plane_seat_id INTEGER        NOT NULL,
+    flight_id     INTEGER        NOT NULL,
+    UNIQUE (plane_seat_id, flight_id),
+    FOREIGN KEY (plane_seat_id) REFERENCES plane_seat (id),
+    FOREIGN KEY (flight_id) REFERENCES flight (id)
+);
+
+CREATE TABLE reservation
+(
+    id        SERIAL PRIMARY KEY,
+    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    user_id   INTEGER   NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES _user (id)
+);
+
+CREATE TABLE reservation_seat
+(
+    id              SERIAL PRIMARY KEY,
+    seats_count     INTEGER NOT NULL,
+    reservation_id  INTEGER NOT NULL,
+    seat_pricing_id INTEGER NOT NULL,
+    UNIQUE (reservation_id, seat_pricing_id),
+    FOREIGN KEY (reservation_id) REFERENCES reservation (id),
+    FOREIGN KEY (seat_pricing_id) REFERENCES seat_pricing (id)
+);
+
+CREATE TABLE discount
+(
+    id              SERIAL PRIMARY KEY,
+    percentage      NUMERIC(5, 2) NOT NULL,
+    seats_count     INTEGER       NOT NULL,
+    seat_pricing_id INTEGER       NOT NULL UNIQUE,
+    FOREIGN KEY (seat_pricing_id) REFERENCES seat_pricing (id)
+);
+
+CREATE TABLE settings
+(
+    id                     SERIAL PRIMARY KEY,
+    min_reservation_hours  INTEGER   NOT NULL,
+    min_cancellation_hours INTEGER   NOT NULL,
+    modified_by            INTEGER REFERENCES _user (id),
+    modified_at            TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
