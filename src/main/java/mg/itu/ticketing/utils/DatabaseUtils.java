8bb59d0503438023bd636cfd1b4926539e200a33
Kevin-rm <kevinramarozatovo@gmail.com>
2025-02-19 14:08:01 +0300

    correct a bug on the emf

--- src/main/java/mg/itu/ticketing/utils/DatabaseUtils.java
+++ src/main/java/mg/itu/ticketing/utils/DatabaseUtils.java
@@ -1,13 +1,8 @@
 package mg.itu.ticketing.utils;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.Persistence;
-import jakarta.persistence.spi.PersistenceProvider;
+import jakarta.persistence.*;
 import mg.matsd.javaframework.core.utils.Assert;
 import mg.matsd.javaframework.core.utils.StringUtils;
-import org.hibernate.jpa.HibernatePersistenceProvider;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,12 +10,10 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 public final class DatabaseUtils {
-    public  static final EntityManagerFactory ENTITY_MANAGER_FACTORY;
-    private static final PersistenceProvider PERSISTENCE_PROVIDER = new HibernatePersistenceProvider();
-    private static final Map<String, String> PROPERTIES;
+    public static final EntityManagerFactory ENTITY_MANAGER_FACTORY;
 
     static {
-        PROPERTIES = new HashMap<>();
+        Map<String, String> additionalProperties = new HashMap<>();
 
         final String databaseUrl = System.getenv("DATABASE_URL");
         final String databaseUsername = System.getenv("DATABASE_USERNAME");
@@ -31,17 +24,11 @@ public final class DatabaseUtils {
             !StringUtils.isNullOrBlank(databasePassword),
             "Les variables d'environnement de la base de données ne sont pas définies");
 
-        PROPERTIES.put("hibernate.connection.url", databaseUrl);
-        PROPERTIES.put("hibernate.connection.username", databaseUsername);
-        PROPERTIES.put("hibernate.connection.password", databasePassword);
-        PROPERTIES.put("hibernate.connection.driver_class", "org.postgresql.Driver");
-        PROPERTIES.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        PROPERTIES.put("hibernate.show_sql", "true");
-        PROPERTIES.put("hibernate.format_sql", "true");
-        PROPERTIES.put("hibernate.physical_naming_strategy",
-            "org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy");
+        additionalProperties.put("hibernate.connection.url", databaseUrl);
+        additionalProperties.put("hibernate.connection.username", databaseUsername);
+        additionalProperties.put("hibernate.connection.password", databasePassword);
 
-        ENTITY_MANAGER_FACTORY = PERSISTENCE_PROVIDER.createEntityManagerFactory("default", PROPERTIES);
+        ENTITY_MANAGER_FACTORY = Persistence.createEntityManagerFactory("default", additionalProperties);
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             if (ENTITY_MANAGER_FACTORY == null || !ENTITY_MANAGER_FACTORY.isOpen()) return;
