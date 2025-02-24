46ecb04cf6b8788d4ce4723442d9babdd570eca5
Kevin-rm <kevinramarozatovo@gmail.com>
2025-02-24 22:52:32 +0300

    settings controller

---
+++ src/main/java/mg/itu/ticketing/controller/SettingsController.java
@@ -0,0 +1,57 @@
+package mg.itu.ticketing.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import mg.itu.prom16.annotations.*;
+import mg.itu.prom16.base.Model;
+import mg.itu.prom16.base.RedirectData;
+import mg.itu.prom16.utils.AuthFacade;
+import mg.itu.prom16.validation.ModelBindingResult;
+import mg.itu.ticketing.entity.User;
+import mg.itu.ticketing.request.SettingsRequest;
+import mg.itu.ticketing.service.SettingsService;
+import mg.itu.ticketing.utils.DatabaseUtils;
+import mg.matsd.javaframework.security.annotation.Authorize;
+import mg.matsd.javaframework.validation.annotations.Validate;
+
+@Log4j2
+@RequiredArgsConstructor
+@Authorize("ADMIN")
+@Controller
+@RequestMapping("/backoffice/parametres")
+public class SettingsController {
+    private final SettingsService settingsService;
+
+    @Get
+    public String showForm(Model model) {
+        DatabaseUtils.execute(entityManager -> model.addData("s",
+            SettingsRequest.fromSettings(settingsService.getFirstOrNew(entityManager))
+        ));
+
+        return "/views/backoffice/settings-form";
+    }
+
+    @Post
+    public String handleForm(
+        @Validate @ModelData("s") SettingsRequest settingsRequest,
+        ModelBindingResult modelBindingResult,
+        RedirectData redirectData
+    ) {
+        if (modelBindingResult.hasErrors()) {
+            redirectData.addAll(modelBindingResult.getFieldErrorsMap());
+            redirectData.add("s", settingsRequest);
+        }
+
+        try {
+            DatabaseUtils.executeTransactional(entityManager ->
+                settingsService.save(settingsRequest, entityManager, (User) AuthFacade.getCurrentUser()));
+            redirectData.add("success", "Paramètres enregistrés avec succès");
+        } catch (Exception e) {
+            log.error("Erreur lors d'un enregistrement des paramètres", e);
+            redirectData.add("error", "Erreur lors de l'enregistrement des paramètres");
+            redirectData.add("s", settingsRequest);
+        }
+
+        return "redirect:/backoffice/parametres";
+    }
+}
--- src/main/java/mg/itu/ticketing/service/SettingsService.java
+++ src/main/java/mg/itu/ticketing/service/SettingsService.java
@@ -1,6 +1,7 @@
 package mg.itu.ticketing.service;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
 import mg.itu.ticketing.entity.Settings;
 import mg.itu.ticketing.entity.User;
 import mg.itu.ticketing.request.SettingsRequest;
@@ -9,12 +10,21 @@ import mg.matsd.javaframework.core.annotations.Component;
 @Component
 public class SettingsService {
 
+    public Settings getFirstOrNew(final EntityManager entityManager) {
+        try {
+            return entityManager.createQuery("SELECT s FROM Settings s ORDER BY s.id", Settings.class)
+                .setMaxResults(1)
+                .getSingleResult();
+        } catch (NoResultException e) {
+            return new Settings();
+        }
+    }
+
     public void save(
-        final Settings settings,
-        final SettingsRequest request,
-        final EntityManager entityManager,
-        final User user
+        final SettingsRequest request, final EntityManager entityManager, final User user
     ) {
+        final Settings settings = getFirstOrNew(entityManager);
+
         settings.setMinReservationHours(request.getMinReservationHours());
         settings.setMinCancellationHours(request.getMinCancellationHours());
         settings.setModifiedBy(user);
