c7c6a814f8a4e5b998f1ef96f97dcfc46a20009a
Kevin-rm <kevinramarozatovo@gmail.com>
2025-04-14 17:27:08 +0300

    move home to frontoffice

--- src/main/java/mg/itu/ticketing/controller/PublicController.java
+++ src/main/java/mg/itu/ticketing/controller/PublicController.java
@@ -2,12 +2,13 @@ package mg.itu.ticketing.controller;
 
 import mg.itu.prom16.annotations.Controller;
 import mg.itu.prom16.annotations.Get;
+import mg.itu.ticketing.utils.ApplicationConstants;
 
 @Controller
 public class PublicController {
 
     @Get
     public String home() {
-        return "/views/home";
+        return ApplicationConstants.Views.FRONTOFFICE_PREFIX +  "home";
     }
 }
