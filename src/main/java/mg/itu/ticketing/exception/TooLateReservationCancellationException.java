57e419d419b0ac3687565c95ab2e884568b651fc
Kevin-rm <kevinramarozatovo@gmail.com>
2025-04-28 08:29:38 +0300

    Update TooLateReservationCancellationException.java

--- src/main/java/mg/itu/ticketing/exception/TooLateReservationCancellationException.java
+++ src/main/java/mg/itu/ticketing/exception/TooLateReservationCancellationException.java
@@ -7,8 +7,8 @@ public class TooLateReservationCancellationException extends RuntimeException {
     private final Integer minCancellationHours;
 
     public TooLateReservationCancellationException(Integer minCancellationHours) {
-        super(String.format("Les annulations ne sont plus possibles après %d heure%s suivant la réservation",
-            minCancellationHours, minCancellationHours > 1 ? "s" : ""));
+        super(String.format("Les annulations ne sont plus possibles après %d heure%c suivant la réservation",
+            minCancellationHours, minCancellationHours > 1 ? 's' : '\0'));
         this.minCancellationHours = minCancellationHours;
     }
 }
